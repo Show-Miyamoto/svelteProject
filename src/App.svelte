31fb2bfed3c276c092ff16c84e26f042c261afc7
4次元の 林檎 <4jigennoringo@4jigennonoMac-mini.local>
2024-02-17 23:04:17 +0900

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,50 @@
+<!-- npm run devで実行。localhost:8080で確認 -->
+<script>
+	import ToDoInputForm from "./ToDoInputForm.svelte"
+	import ToDoList from "./ToDoList.svelte"
+	let nameEntered = false  // 名前が入力されたかどうかのフラグ。false->if文のelseが実行
+
+	let lastName = ""
+	let firstName = ""
+
+	$: fullName = lastName + " " + firstName
+</script>
+
+<main>
+{#if nameEntered}
+	<h1>タスクリストアプリケーションへようこそ、{fullName}さん！！</h1>
+	<ToDoInputForm userName={fullName} />
+	<ToDoList />
+{:else}
+	<h1>タスクリストアプリケーションへようこそ！！</h1>
+	<h3>名前を入力してください</h3>
+	<div>
+		<input bind:value={lastName} type="text" placeholder="姓" required>
+	</div>
+	<div>
+		<input bind:value={firstName} type="text" placeholder="名" required>
+	</div>
+{/if}
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 450px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: blue;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
